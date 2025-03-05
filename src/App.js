7f40b2254ef83cb4c7f1c51ae6b354bab8a6513a
MieliGit <zdan14@mail.ru>
2025-03-05 22:31:02 +0300

    end project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+
+  const [count, setCount] = React.useState(0);
+  
+  const onClickPlus = () =>{
+    setCount(count + 1)
+  }
+
+  const onClickMinus = () =>{
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
+        <h1 className='App__h1'> Счетчик: </h1>
+        <span className='App__p'>{count}</span>
+        <div className='App__buttonbox'>
+          <button className='App__buttonbox-buttonplus' onClick={onClickPlus}> +1 </button>
+          <button className='App__buttonbox-buttonminus' onClick={onClickMinus}> -1 </button>
+        </div>
     </div>
   );
 }
